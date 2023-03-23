329fb298b387e5dff97a0a8f6d5e587fd026a3de
lhgioia <lhgioia@gmail.com>
2023-03-23 13:02:46 -0700

    Add warning if passwords do not match

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Check that passwords match
+const pwd1 = document.getElementById("password");
+const pwd2 = document.getElementById("confirm-password");
+
+const span1 = document.getElementById("input-hint-password");
+
+pwd2.addEventListener("input", (event) => {
+  if (pwd1.value !== pwd2.value) {
+    span1.textContent = "* Passwords do not match";
+    span1.className = "no-match";
+    pwd2.style.marginBottom = 0;
+  } else {
+    span1.textContent = "";
+    span1.classList.remove("no-match");
+  }
+});
